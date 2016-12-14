2417f70b462381927588cb6f80054049600a99e1
Russell <russell.tarpley@gmail.com>
2016-12-13 19:22:26 -0600

    dev(slideout)
    Initial project build out: build, config, doc, dev

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
 # mdl-slideout
-A Material Design Lite implementation of a slide out pane
+An Material Design Lite slideout pane implementation (https://github.com/google/material-design-lite)
+
+[![Bower Version](https://img.shields.io/bower/v/mdl-slideout.svg)](https://github.com/rathxxx/mdl-slideout)
+[![NPM Version](https://img.shields.io/npm/v/mdl-slideout.svg)](https://www.npmjs.com/package/mdl-slideout)
+[![license](https://img.shields.io/badge/license-MIT-brightgreen.svg?style=flat)](https://github.com/rathxxx/mdl-slideout/blob/master/LICENSE)
+
+[![bitHound Overall Score](https://www.bithound.io/github/rathxxx/mdl-slideout/badges/score.svg)](https://www.bithound.io/github/rathxxx/mdl-slideout)
+[![bitHound Dependencies](https://www.bithound.io/github/rathxxx/mdl-slideout/badges/dependencies.svg)](https://www.bithound.io/github/rathxxx/mdl-slideout/master/dependencies/npm)
+[![bitHound Dev Dependencies](https://www.bithound.io/github/rathxxx/mdl-slideout/badges/devDependencies.svg)](https://www.bithound.io/github/rathxxx/mdl-slideout/master/dependencies/npm)
+[![bitHound Code](https://www.bithound.io/github/rathxxx/mdl-slideout/badges/code.svg)](https://www.bithound.io/github/rathxxx/mdl-slideout)
+
+> A custom implementation of a slideout panel for [Material Design Lite](https://github.com/google/material-design-lite)
+
+## Install
+
+Via npm:
+
+````
+npm install mdl-slideout
+````
+
+Then include in your html:
+
+````
+<link rel="stylesheet" href="dist/mdl-slideout.min.css">
+...
+<script src="dist/mdl-slideout.min.js"></script>
+````
+
+## Basic use
+To use any MDL component, you must include the minified CSS and JavaScript files using standard relative-path references in the `<head>` section of the page, as described in the MDL Introduction.
+
+### To include a MDL **date textfield** component:
+
+&nbsp;1. Code a `<div>` element to hold the date text field.
+```html
+<div>
+...
+</div>
+```
---
+++ src/slideout/slideout.js
@@ -0,0 +1,96 @@
+(function() {
+    'use strict';
+
+    /**
+     * Class constructor for Slideout MDL component.
+     * Implements MDL component design pattern defined at:
+     * https://github.com/jasonmayes/mdl-component-design-pattern
+     *
+     * @constructor
+     * @param {HTMLElement} element The element that will be upgraded.
+     */
+    var MaterialSlideout = function MaterialSlideout(element) {
+        this.element_ = element;
+        // Initialize instance.
+        this.init();
+    };
+    window['MaterialSlideout'] = MaterialSlideout;
+
+    /**
+     * Store constants in one place so they can be updated easily.
+     *
+     * @enum {string | number}
+     * @private
+     */
+    MaterialSlideout.prototype.Constant_ = {};
+
+    /**
+     * Store strings for class names defined by this component that are used in
+     * JavaScript. This allows us to simply change it in one place should we
+     * decide to modify at a later date.
+     *
+     * @enum {string}
+     * @private
+     */
+    MaterialSlideout.prototype.CssClasses_ = {
+        OVERLAY: 'mdl-slideout__overlay',
+        TITLE: 'mdl-slideout__title',
+        ACTIONS: 'mdl-slideout__actions',
+        IS_ACTIVE: 'is-active'
+    };
+
+    /**
+   * Handle input being entered.
+   *
+   * @param {Event} event The event that fired.
+   * @private
+   */
+    MaterialSlideout.prototype.onKeyDown_ = function (event) {
+
+    };
+
+    // Public methods.
+
+    /**
+     * Show the slideout (Make Active)
+     *
+     * @public
+     */
+    MaterialSlideout.prototype.show = function () {
+        this.element_.classList.add(this.CssClasses_.IS_ACTIVE);
+        this.overlay_.classList.add(this.CssClasses_.IS_ACTIVE);
+    };
+    MaterialSlideout.prototype['show'] = MaterialSlideout.prototype.show;
+
+    /**
+     * Hide the slideout (Remove Active)
+     *
+     * @public
+     */
+    MaterialSlideout.prototype.hide = function () {
+        this.element_.className = this.element_.className.replace('.' + this.CssClasses_.IS_ACTIVE);
+        this.element_.className = this.overlay_.className.replace('.' + this.CssClasses_.IS_ACTIVE);
+    };
+    MaterialSlideout.prototype['hide'] = MaterialSlideout.prototype.hide;
+
+    /**
+    * Initialize element.
+    */
+    MaterialSlideout.prototype.init = function () {
+
+        if (this.element_) {
+            this.overlay_ = this.element_.querySelector('.' + this.CssClasses_.OVERLAY);
+            this.title_ = this.element_.querySelector('.' + this.CssClasses_.TITLE);
+            this.actions_ = this.element_.querySelector('.' + this.CssClasses_.ACTIONS);
+        }
+    };
+
+    // The component registers itself. It can assume componentHandler is available
+    // in the global scope.
+    componentHandler.register({
+        constructor: MaterialSlideout,
+        classAsString: 'MaterialSlideout',
+        cssClass: 'mdl-js-slideout',
+        widget: true
+    });
+})();
